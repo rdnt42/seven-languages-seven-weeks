504413985805884754c10826bdf8b883261a353d
marowak <radiant42@yandex.ru>
2023-06-20 00:57:37 +0400

    added kata prolog

--- prolog-3-week/kata.pl
+++ prolog-3-week/kata.pl
@@ -1,21 +1,27 @@
 %my
-binary_search(List, Target, Index) :-
-    get_middle(List, Middle),
+binary_search(List, Target, StartIndex, ResultIndex) :-
+    get_middle(List, Middle, _),
     dif(Middle, Target),
     split_list(List, Left, Right),
-    (Middle > Target -> binary_search(Left, Target, Index); binary_search(Right, Target, Index)).
+    length(Left, N),
+    (Middle > Target ->
+        binary_search(Left, Target, StartIndex, ResultIndex);
+        NewStartIndex is (StartIndex + N),
+        binary_search(Right, Target, NewStartIndex, ResultIndex)
+    ).
 
-binary_search(List, Target, _) :-
-    get_middle(List, Middle),
-    Middle = Target.
+binary_search(List, Target, StartIndex, ResultIndex) :-
+    get_middle(List, Middle, MiddleIndex),
+    Middle = Target,
+    ResultIndex is (StartIndex + MiddleIndex - 1).
 
 %Stack Overflow
-get_middle(List, Middle) :-
+get_middle(List, Middle, Index) :-
     List = [_|_],           % at least one element
     length(List, Len),
     divmod(Len, 2, Q, R),   % if not available do in two separate steps
-    N is Q + R,
-    nth1(N, List, Middle).
+    Index is Q + R,
+    nth1(Index, List, Middle).
 
 %chat gpt
 split_list(List, List1, List2) :-
@@ -28,4 +34,4 @@ split_list([], [], []).    % Base case: If the input list is empty, both output 
 split_list([X], [X], []).  % Special case for a list with a single element: X is added to the first output list, and the second output list remains empty.
 
 %['kata.pl'].
-%binary_search([-1, 0, 3, 5, 9, 12], 9, What).
+%binary_search([-1, 0, 3, 5, 9, 12], 9, 1, What).
