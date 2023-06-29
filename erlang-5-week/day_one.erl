b4eb6ebc57df93b7c5d9cb0c26d155886fac3697
marowak <radiant42@yandex.ru>
2023-06-29 22:50:05 +0400

    added day one homework

--- erlang-5-week/day_one.erl
+++ erlang-5-week/day_one.erl
@@ -2,6 +2,8 @@
 -export([another_factorial/1]).
 -export([another_fib/1]).
 -export([counter/1]).
+-export([message/1]).
+-export([message/2]).
 
 another_factorial(0) -> 1;
 another_factorial(N) -> N * another_factorial(N-1).
@@ -10,7 +12,11 @@ another_fib(0) -> 1;
 another_fib(1) -> 1;
 another_fib(N) -> another_fib(N-1) + another_fib(N-2).
 
-%%1
+%%2
 counter(1) -> 1;
 counter(N) -> io:fwrite("~w~n",[N]), counter(N-1).
+
+%%3
+message(success) -> io:fwrite("success~n").
+message(error, Message) -> io:fwrite("error:~s~n", [Message]).
 
