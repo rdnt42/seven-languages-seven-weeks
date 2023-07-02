a22b709850a96e95f88f9a455115929d603f44f4
marowak <radiant42@yandex.ru>
2023-07-02 21:55:31 +0400

    added day two homework

--- erlang-5-week/day_two.erl
+++ erlang-5-week/day_two.erl
@@ -1,5 +1,6 @@
 -module(day_two).
 -export([get_value/2]).
+-export([get_total_prices/1]).
 
 %%1
 get_value(Key, List) ->
@@ -8,3 +9,10 @@ get_value(Key, List) ->
     false -> not_found
   end.
 %% day_two:get_value(ruby, [{erlang,"afunctionallanguage"}, {ruby,"anOOlanguage"}]).
+
+%%2
+get_total_prices(Cart) -> [{Item, Quantity * Price} || {Item, Quantity, Price} <- Cart].
+%% [{pencil, 4, 0.25}, {pen, 1, 1.20}, {paper, 2, 0.20}]
+
+%%3
+
