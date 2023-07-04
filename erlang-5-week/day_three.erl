f8ae406d81534de6a100415d0002b098db79b373
marowak <radiant42@yandex.ru>
2023-07-04 18:11:46 +0400

    added day three homework

--- erlang-5-week/day_three.erl
+++ erlang-5-week/day_three.erl
@@ -35,3 +35,18 @@ loop_translate() ->
 %%Translator = spawn(fun day_three:loop/0).
 %%Translator ! new.
 %%revolver ! "casa".
+
+%%3
+loop_doctor() ->
+  process_flag(trap_exit, true),
+  receive
+    new ->
+      io:format("Creating and monitoring process.~n"),
+      register(revolver, spawn_link(fun roulette:loop/0)),
+      loop();
+    {'EXIT', From, Reason} ->
+      io:format("The shooter ~p died with reason ~p.", [From, Reason]),
+      io:format(" Restarting. ~n"),
+      self() ! new,
+      loop()
+  end.
