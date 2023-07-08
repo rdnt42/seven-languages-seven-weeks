6f7539f4c7ad99f4012603afde4abc2087c6d724
marowak <radiant42@yandex.ru>
2023-07-08 22:15:38 +0400

    added second day clojure

--- clojure-6-week/src/clojure_6_week/day_two.clj
+++ clojure-6-week/src/clojure_6_week/day_two.clj
@@ -26,3 +26,20 @@
 (def duck-two (ForestDuck. "Takeshi"))
 (quack duck-one)
 (quack duck-two)
+
+;3
+(defn game []
+
+  (def rnd (rand-int 10))
+   (def input read-line)
+   (println "choose number between 1 and 10")
+   (while (input not= rnd)
+     cond (
+           (> rnd input) (println "Your number lower than answer")
+           (< rnd input) (println "Your number greater than answer")
+           )
+     )
+   (println "You won!")
+
+  )
+(game)
