f76d66d09607d2b19796561e5cfe7c7b5f1bd397
marowak <radiant42@yandex.ru>
2023-07-20 18:51:25 +0400

    added haskel day two homework

--- haskel-7-week/daytwo.hs
+++ haskel-7-week/daytwo.hs
@@ -5,8 +5,28 @@ quicksort (x:xs) =
         biggerSorted = quicksort [a | a <- xs, a > x]
     in smallerSorted ++ [x] ++ biggerSorted
 
+halfNumber :: Fractional a => a -> a
+halfNumber = (/ 2)
+
+addNewline :: String -> String
+addNewline = (++ "\n")
+
 main :: IO ()
 main = do
   let unsortedList = [4, 2, 9, 5, 1, 8, 3]
   let sortedList = quicksort unsortedList
   putStrLn $ "Sorted list: " ++ show sortedList
+
+  let text = "text"
+  putStr text
+  putStr text
+  putStr (addNewline text)
+  putStrLn "Text in new line"
+
+  let num = 98.0 :: Double
+  let half = halfNumber num
+  putStrLn $ "Half of " ++ show num ++ " is " ++ show half
+
+
+
+
